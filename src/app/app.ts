9ea5ce088cff53b4449b3713a2b3ed2f6430b4f8
StefanoR <rubinetti.stefano@gmail.com>
2025-09-29 16:28:08 +0200

    esempio volontari

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Volontari} from './components/volontari/volontari';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Volontari],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/model/Volontario.ts
@@ -0,0 +1,6 @@
+export interface Volontario
+{
+  id:string,
+  nominativo:string,
+  visible:boolean;
+}
